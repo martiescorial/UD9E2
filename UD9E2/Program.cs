6b2a9d6a6007aad3d4369c39864402ccb527d1c1
Martina <martinaescorial9@gmail.com>
2021-07-03 14:37:19 +0200

    Add project files.

---
+++ UD9E2/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace UD9E2
+{
+    class Empleado
+    {
+        int sueldo;
+
+        string nombre;
+        public Empleado()
+        {
+            Console.WriteLine("Introduzca el nombre del empleado");
+            nombre = Console.ReadLine();
+            Console.WriteLine("Introduzca el sueldo del empleado");
+            string linea = Console.ReadLine();
+            sueldo = int.Parse(linea);
+        }
+
+        public void Mostrar()
+        {
+            Console.WriteLine("Nombre:" + nombre);
+            Console.WriteLine("Sueldo:" + sueldo);
+        }
+
+        public void Impuestos()
+        {
+            if (sueldo >= 3000)
+            {
+                Console.WriteLine(nombre + " debe pagar impuestos.");
+            }
+            else
+            {
+                Console.WriteLine(nombre + " no debe pagar impuestos.");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Empleado empleado1 = new Empleado();
+            empleado1.Mostrar();
+            empleado1.Impuestos();
+            Console.ReadKey();
+        }
+    }
+}
